3f80bde056aab696a9301c6002b1cf931f6bbd4f
JNickolasz <jose.nickolas.paiva@gmail.com>
2026-04-20 17:34:32 -0300

    Feat: admin credentials via environment variables

--- Server/core/management/commands/criar_admin.py
+++ Server/core/management/commands/criar_admin.py
@@ -1,9 +1,13 @@
 from django.contrib.auth.models import User
 from django.core.management.base import BaseCommand
-import os
+from decouple import config
 
 class Command(BaseCommand):
     def handle(self, *args, **kwargs):
-        if not User.objects.filter(username='josenickolas').exists():
-            User.objects.create_superuser('josenickolas', os.environ.get('EMAIL_ADDRESS'), 'sitoio7593')
+        if not User.objects.filter(username=config('ADMIN_USER')).exists():
+            User.objects.create_superuser(
+                config('ADMIN_USER'),
+                config('ADMIN_EMAIL'),
+                config('ADMIN_PASSWORD')
+            )
             self.stdout.write('Superusuário criado!')
