12dabc3ea3e78058a86e06154a536a6aae69cb5b
JNickolasz <jose.nickolas.paiva@gmail.com>
2026-04-26 11:55:26 -0300

    Feat:tecnologias grid layout with icons and names

--- Server/core/models.py
+++ Server/core/models.py
@@ -45,6 +45,9 @@ class Tecnologia(models.Model):
     nome = models.CharField(max_length=20)
     icone_url = models.URLField()
 
+    class Meta:
+        ordering = ['ordem']
+
     def __str__(self): 
         return self.nome
     
