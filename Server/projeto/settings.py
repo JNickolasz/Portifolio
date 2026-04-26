2c28c47f61fd3ba8a4518804eb6ac62c7e32c28a
JNickolasz <jose.nickolas.paiva@gmail.com>
2026-04-26 09:12:22 -0300

    Fix: cloudinary storage config and middleware order

--- Server/projeto/settings.py
+++ Server/projeto/settings.py
@@ -54,7 +54,7 @@ INSTALLED_APPS = [
     "core",
     "portifolio",
     "cloudinary",
-    "cloudinary-storage",
+    "cloudinary_storage",
 ]
 
 DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
@@ -67,14 +67,13 @@ CLOUDINARY_STORAGE = {
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware", 
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
-    "whitenoise.middleware.WhiteNoiseMiddleware", 
-    "django.middleware.security.SecurityMiddleware",
 ]
 
 ROOT_URLCONF = "projeto.urls"
