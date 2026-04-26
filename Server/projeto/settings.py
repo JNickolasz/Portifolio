03b69d8c9ca56bdf00c0670616c0a1c41e2f4a03
JNickolasz <jose.nickolas.paiva@gmail.com>
2026-04-26 09:02:01 -0300

    Fix: typo in cloudinary cloud name config

--- Server/projeto/settings.py
+++ Server/projeto/settings.py
@@ -60,7 +60,7 @@ INSTALLED_APPS = [
 DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 
 CLOUDINARY_STORAGE = {
-    'CLOUDE_NAME' : config('CLOUDINARY_CLOUDE_NAME'),
+    'CLOUD_NAME' : config('CLOUDINARY_CLOUD_NAME'),
     'API_KEY' : config('CLOUDINARY_API_KEY'),
     'API_SECRET' : config('CLOUDINARY_API_SECRET'),
 }
