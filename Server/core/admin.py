2ac3c91b544190d79bc169bf303b57dbaa22026b
JNickolasz <jose.nickolas.paiva@gmail.com>
2026-04-26 11:41:31 -0300

    Fix: register tecnologia model in admin

--- Server/core/admin.py
+++ Server/core/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from .models import DadosPessoais, Projeto, Certificado
+from .models import DadosPessoais, Projeto, Certificado,Tecnologia
 
 # Register your models here.
 
 admin.site.register(DadosPessoais)
 admin.site.register(Projeto)
 admin.site.register(Certificado)
+admin.site.register(Tecnologia)
